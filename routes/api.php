c2c1422091334df7a03e6927c13572b48a22c641
taufiq <htaufiq225@gmail.com>
2026-06-23 19:15:53 +0800

    feat: initialize API V1 route structure with authentication and resource endpoints

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
 
+    Route::get('/test', function () {
+        return response()->json([
+            'message' => 'Success'
+        ]);
+    });
+
     // Auth Routes
     Route::prefix('auth')->group(function () {
         // Public Auth
